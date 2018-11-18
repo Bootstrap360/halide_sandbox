c25054a19e14c3571490fdadd3518d69614f7708
Glen Turner <turner.glen@gmail.com>
2018-11-18 12:50:39 -0800

    Up to tutorial 3!

---
+++ tutorials/include/03.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "Halide.h"
+#include <stdio.h>
+
+
+void tutorial_03()
+{
+    using namespace Halide;
+    // We'll start by defining the simple single-stage imaging
+    // pipeline from lesson 1.
+
+    // This lesson will be about debugging, but unfortunately in C++,
+    // objects don't know their own names, which makes it hard for us
+    // to understand the generated code. To get around this, you can
+    // pass a string to the Func and Var constructors to give them a
+    // name for debugging purposes.
+    Func gradient("gradient");
+    Var x("x"), y("y");
+    gradient(x, y) = x + y;
+
+    // Realize the function to produce an output image. We'll keep it
+    // very small for this lesson.
+    Buffer<int> output = gradient.realize(8, 8);
+
+    // That line compiled and ran the pipeline. Try running this
+    // lesson with the environment variable HL_DEBUG_CODEGEN set to
+    // 1. It will print out the various stages of compilation, and a
+    // pseudocode representation of the final pipeline.
+    // Click to show output ...
+
+    // If you set HL_DEBUG_CODEGEN to a higher number, you can see
+    // more and more details of how Halide compiles your pipeline.
+    // Setting HL_DEBUG_CODEGEN=2 shows the Halide code at each stage
+    // of compilation, and also the llvm bitcode we generate at the
+    // end.
+
+    // Halide will also output an HTML version of this output, which
+    // supports syntax highlighting and code-folding, so it can be
+    // nicer to read for large pipelines. Open gradient.html with your
+    // browser after running this tutorial.
+    gradient.compile_to_lowered_stmt("tutorial_02_gradient.html", {}, HTML);
+
+    printf("tutorial_03 success!\n");
+}
---
+++ tutorials/main.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+
+#define SHOW(var) std::cout << #var << " = " << var << std::endl
+
+
+#include "01.h"
+#include "02.h"
+#include "03.h"
+
+
+int main(int argc, char **argv)
+{
+    // lessons can be found here http://halide-lang.org/tutorials/tutorial_introduction.html
+
+    tutorial_01();
+    for( auto gain : {0.2, 0.5, 1.5, 10.0} )
+    {
+        tutorial_02(gain);
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    }
+    tutorial_03();
+
+
+    return 0;
+
+}
